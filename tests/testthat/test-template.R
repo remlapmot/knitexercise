f067230f9e2fdf0e3a521cb8bcf0d5b49c37b292
Tom Palmer <remlapmot@hotmail.com>
2020-05-09 23:23:37 +0100

    Update test-template.R

--- tests/testthat/test-template.R
+++ tests/testthat/test-template.R
@@ -5,16 +5,13 @@ test_that("rendering the template successfully
 
 	skip_on_cran()
 
-	templatermd <- I(tempfile())
-	rmarkdown::draft(file = templatermd,
-									 template = "exercise_template",
-									 package = "knitexercise",
-									 create_dir = FALSE,
-									 edit = FALSE)
 	output_file <- I(tempfile())
-  rmarkdown::render(input = templatermd,
-  									output_file = output_file,
-  									quiet = TRUE)
+	on.exit(unlink(output_file), add = TRUE)
+
+	template_path <- "../../inst/rmarkdown/templates/exercise_template/skeleton/skeleton.Rmd"
+	rmarkdown::render(input = template_path,
+  									quiet = TRUE,
+										output_file = output_file)
   expect_true(file.exists(output_file))
  }
 )
