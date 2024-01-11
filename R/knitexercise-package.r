6b4a1e6f25dbf2fa1513f6cb4de2c50aae9e5046
Tom Palmer <remlapmot@hotmail.com>
2024-01-11 15:11:48 +0000

    Edits and formatting

--- R/knitexercise-package.r
+++ R/knitexercise-package.r
@@ -1,11 +1,11 @@
 #' knitexercise: Knit exercise questions and solutions from the same R Markdown document
 #'
-#' This package provides a custom knit function, `knit_exercise`, which sets the global
+#' This package provides a custom knit function, `knit_exercise()`, which sets the global
 #' chunk option `include`. This is controlled by setting the `solutions` parameter in
 #' the YAML header of the R Markdown document.
 #'
-#' Hence to generate your output document with questions set `solutions: FALSE`.
-#' For your output document with your solutions set `solutions: TRUE`.
+#' To generate your output document with questions set `solutions: FALSE`.
+#' For your output document containing the solutions set `solutions: TRUE`.
 #'
 #' Your YAML header should look something like the following.
 #' ```yaml
@@ -23,12 +23,14 @@
 #' the file extension.
 #'
 #' There is a template R Markdown document which you can open through the 
-#' following menu: *File | New File | R Markdown... | From Template*, 
+#' following menu in RStudio: *File | New File | R Markdown... | From Template*, 
 #' or with the following code.
 #' ```r
-#' rmarkdown::draft("my_exercise.Rmd", 
-#'                  template = "exercise_template", 
-#'                  package = "knitexercise")
+#' rmarkdown::draft(
+#'   "my_exercise.Rmd",
+#'   template = "exercise_template",
+#'   package = "knitexercise"
+#' )
 #' ```
 #'
 #' @keywords internal
