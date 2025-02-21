92c8f4cc8d4a79f06ff9aed592c481346420c449
Tom Palmer <remlapmot@hotmail.com>
2025-02-21 11:53:42 +0000

    Update test-template.R

--- tests/testthat/test-template.R
+++ tests/testthat/test-template.R
@@ -37,5 +37,22 @@ test_that("Test knitexercise::knit_exercise() works", {
 						if (file.exists(output_file)) file.remove(output_file)
 						knit_exercise(template_path)
 						expect_true(file.exists(output_file))
+						if (file.exists(output_file)) file.remove(output_file)
 					}
+)
+
+test_that("Test knitexercise::knit_exercise() works with params: solutions: TRUE in YAML header", {
+	
+	skip_on_cran()
+	
+	template_path <- system.file("rmarkdown",
+															 "test-solutions.Rmd",
+															 package = "knitexercise")
+	
+	output_file <- paste0(tools::file_path_sans_ext(template_path), "-solutions.html")
+	if (file.exists(output_file)) file.remove(output_file)
+	knit_exercise(template_path)
+	expect_true(file.exists(output_file))
+	if (file.exists(output_file)) file.remove(output_file)
+}
 )
